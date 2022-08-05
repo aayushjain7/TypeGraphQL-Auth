d865837b2887d268f89ce18381c2674830a23315
Aayush <jaayush014@gmail.com>
2022-08-06 02:12:30 +0530

    login and cookies

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,61 @@ import { ApolloServer } from "apollo-server-express";
 import express, { Express } from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+// import cors from "cors";
 
+import { redis } from "./redis";
 import { RegisterResolver } from "./modules/user/Register";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 const main = async () => {
+	const isProduction = process.env.NODE_ENV === "production";
+
 	await createConnection();
 	const schema = await buildSchema({
-		resolvers: [RegisterResolver],
+		resolvers: [MeResolver, RegisterResolver, LoginResolver],
 	});
 
-	const apolloServer = new ApolloServer({ schema });
+	const apolloServer = new ApolloServer({
+		schema,
+		context: ({ req }: any) => ({ req }),
+	});
 
 	const app: Express = express();
 
+	const RedisStore = connectRedis(session);
+
+	!isProduction && app.set("trust proxy", 1);
+
+	app.use(
+		session({
+			store: new RedisStore({
+				client: redis,
+			}),
+			name: "qid",
+			secret: "sdijdncjansj23239slmas",
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				httpOnly: true,
+				// secure: process.env.NODE_ENV === "production",
+				maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+				sameSite: "none",
+				secure: true,
+			},
+		} as any)
+	);
+
 	await apolloServer.start();
-	apolloServer.applyMiddleware({ app });
+	apolloServer.applyMiddleware({
+		app,
+		cors: {
+			origin: ["https://studio.apollographql.com"],
+			credentials: true,
+		},
+	});
 
 	app.listen(4000, () => {
 		console.log("server started on http://localhost:4000/graphql");
---
+++ src/modules/user/Me.ts
@@ -0,0 +1,16 @@
+import { MyContext } from "src/types/MyContext";
+import { Ctx, Query, Resolver } from "type-graphql";
+
+import { User } from "../../entity/User";
+
+@Resolver()
+export class MeResolver {
+	@Query(() => User, { nullable: true })
+	async me(@Ctx() ctx: MyContext): Promise<User | null> {
+		console.log(ctx.req.session.userId);
+		if (!ctx.req.session.userId) {
+			return null;
+		}
+		return User.findOne({ where: { id: ctx.req.session!.userId } });
+	}
+}
